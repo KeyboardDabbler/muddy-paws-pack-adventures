63312c56f152402dd116a765e1027296d0bfe3d4
keyboarddabbler <keyboarddabbler@gmail.com>
2023-10-13 16:46:51 +1300

    feat: css teme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: [],
   theme: {
+    colors: {
+      backg: '#35AC90',
+      forg: '#35AC90',
+      action: '#62466B',
+      stroke: '#73D4BD',
+      focus: '#2B8C76',
+      yellow: '#f4cf44',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
