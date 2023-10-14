86ef7c7d517993992a60ed624276d830cce2fcac
keyboarddabbler <keyboarddabbler@gmail.com>
2023-10-14 16:22:39 +1300

    update: use NuxtPage routes

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="antialiased duration-300 transition-colors text-gray-400 bg-backg">
+    <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+    <NuxtPage />
   </div>
 </template>
